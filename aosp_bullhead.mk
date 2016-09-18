b2f4f4a3c191a7f622a969ab70106177801c6442
Victor Lapin <victor@slimroms.org>
2016-09-18 18:57:19 +0300

    Update build fingerprint to NRD90S

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -44,3 +44,7 @@ $(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
 PRODUCT_PACKAGES += \
     Launcher3
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD90S/3142244:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90S 3142244 release-keys"
+
